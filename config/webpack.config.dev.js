c184db36f07d3e4dea47eb2e576d5c79eafb22e7
andoyoung <jis_poop@weneepl.com>
2018-08-28 15:58:15 +0900

    webpack-dev-server hot reloading

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -20,6 +21,7 @@ module.exports = {
       template: './src/index.html',
       filename: 'index.html',
       inject: true
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
